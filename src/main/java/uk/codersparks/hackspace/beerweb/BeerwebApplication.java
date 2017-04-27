affaa04a910329b82f3aa4b05989c4450900fc40
codersparks <codersparks@codersparks.uk>
2017-04-27 19:10:56 +0100

    update spring boot application for openshift

--- src/main/java/uk/codersparks/hackspace/beerweb/BeerwebApplication.java
+++ src/main/java/uk/codersparks/hackspace/beerweb/BeerwebApplication.java
@@ -2,11 +2,22 @@ package uk.codersparks.hackspace.beerweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class BeerwebApplication {
+public class BeerwebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeerwebApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<BeerwebApplication> applicationClass = BeerwebApplication.class;
+
 }
